03c350e6231a5de9a7f662cc9e4485cc83e8321f
wuyingqiang <794870409@qq.com>
2020-03-29 23:03:41 +0800

    UPDATE - update code
    ### 更新内容
    
    添加 标识符、判断(>、>=)、数字 的枚举
    
    ### 更新原因
    
    词法分析

---
+++ src/com/unclew/compiler/lexical/TokenType.java
@@ -0,0 +1,15 @@
+package com.unclew.compiler.lexical;
+
+/**
+ * Created by wuyingqiang
+ * on 2020/3/29-10:58 下午.
+ *
+ * @author wuyingqiang
+ * @since 1.0
+ */
+public enum TokenType {
+    IDENTIFY, // 标识符
+    GT, // >
+    GE, // >=
+    DIGIT // 数字
+}
