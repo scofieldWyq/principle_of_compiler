9215ec42233a890f4eb40e382e4199f5e021f48f
wuyingqiang <794870409@qq.com>
2020-03-29 23:11:48 +0800

    UPDATE - update code
    ### 更新内容
    
    创建一个提供字符内容的接口
    
    ### 更新原因
    
    词法分析

---
+++ src/com/unclew/compiler/lexical/WordMachine.java
@@ -0,0 +1,19 @@
+package com.unclew.compiler.lexical;
+
+/**
+ * Created by wuyingqiang
+ * on 2020/3/29-11:04 下午.
+ *
+ * 提供处理的字符集合
+ *
+ * @author wuyingqiang
+ * @since 1.0
+ */
+public interface WordMachine {
+    // read
+    char current(); // 返回当前没有被消费的字符
+    char preview(int offset); // 预览当前位置后第 {offset} 个字符
+
+    // operation
+    void next(int offset); // 滑动到下 {offset} 个字符
+}
